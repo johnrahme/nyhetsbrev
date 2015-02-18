3de94d7d18eb7ee0f2f19c630b47aeb917729023
johnrahme <john.rahme.se@gmail.com>
2015-02-18 18:12:57 +0100

    fastmailererr

--- app/controllers/FastMailController.php
+++ app/controllers/FastMailController.php
@@ -16,17 +16,38 @@ class FastMailController extends \BaseController {
 
         $receivers = array();
         if($rec == 'utb'){
-            $from = 'nyhetsbrev@futf.se';
+            $from = 'john_rahme92@hotmail.com';
             $sendName = 'Utbildningsutskottet-FUTF';
             foreach(utbildningsutskottet::all() as $person){
                 $adress = $person->Email;
-
                 array_push($receivers,$adress);
             }
-
-        $koll = implode(',',$receivers);
-
         }
+        if($rec == 'idrott'){
+            $from = 'john_rahme92@hotmail.com';
+            $sendName = 'Idrottsutskottet-FUTF';
+            foreach(idrottsutskottet::all() as $person){
+                $adress = $person->Email;
+                array_push($receivers,$adress);
+            }
+        }
+        if($rec == 'klubb'){
+            $from = 'john_rahme92@hotmail.com';
+            $sendName = 'Klubbverket-FUTF';
+            foreach(klubbverket::all() as $person){
+                $adress = $person->Email;
+                array_push($receivers,$adress);
+            }
+        }
+        if($rec == 'arb'){
+            $from = 'john_rahme92@hotmail.com';
+            $sendName = 'Arbetsmarknadsutskottet-FUTF';
+            foreach(arbetsmarknadsutskottet::all() as $person){
+                $adress = $person->Email;
+                array_push($receivers,$adress);
+            }
+        }
+        $koll = implode(',',$receivers);
 
         $data = array('utskott' => $rec, 'text'=>$text);
         Mail::send('fastMail.layout.main', $data, function ($message) use ($name, $receivers,$from,$sendName ){
--- app/views/fastMail/layout/main.blade.php
+++ app/views/fastMail/layout/main.blade.php
@@ -1,5 +1,5 @@
 {{$text}}
 <p>
-Om du vill sluta få mail från {{$utskott}} så skicka ett mail till it(at)futf.se
+Om du vill sluta få mail från {{$utskott}} så skicka ett mail till it@futf.se
 
 </p>
